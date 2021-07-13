dea35f51d4e449d73e38e86ae991a566cf775fa7
Cimpean Rares <rares_cimpean@yahoo.com>
2021-07-13 17:38:44 +0300

    first commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+Se va accesa http://127.0.0.1:9000/
---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<form action="action.php" method="post">
+ <p>Introduceti numele <input type="text" name="name" /></p>
+ <p><input type="submit" /></p>
+</form>
+
+</body>
+</html>
